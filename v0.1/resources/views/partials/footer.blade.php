edf3da1dde3e7ed1658b97683050cd88b257edd0
Peibol IdeaPad <informaticacgtferroviario@gmail.com>
2022-07-02 13:17:00 +0200

    refinada la plantilla

--- v0.1/resources/views/partials/footer.blade.php
+++ v0.1/resources/views/partials/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="pt-5 my-5 text-muted">
-    Creado por<a href="">CGT Ferroviario</a> &middot; &copy; {{ date('Y') }}
+    Creado por <a href="">CGT Ferroviario</a> &middot; &copy; {{ date('Y') }}
   </footer>
--- v0.1/resources/views/welcome.blade.php
+++ v0.1/resources/views/welcome.blade.php
@@ -3,11 +3,45 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="description" content="">
+        <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
         <title>CGT Ferroviario</title> 
-
+        @include('partials.styles')
     </head>
     <body class="antialiased">
-        
+      @include('partials.header')
+
+      <main class="container mt-5">
+        @yield('content')
+      </main>
+    
+      @include('partials.footer')
+    
+      @include('partials.scripts')            
+
     </body>
 </html>
+
+{{-- @extends('layouts.master')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection --}}
