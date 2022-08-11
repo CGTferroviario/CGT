3f4367e00d6efd42542db088485736797f02e1ee
Don Peibol <elcorreodedonpablo@gmail.com>
2022-08-11 16:31:24 +0200

    Añadido Blog

---
+++ blog/storage/framework/views/4d649ea10505a580d50a02b6af40dbc363218fa9.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+    <title>Mi Blog</title>
+    <link rel="stylesheet" href="/app.css">
+    <script src="/app.js"></script>
+</head>
+
+<body>
+    <h1>Mi Blog</h1>
+</body>
+
+<?php /**PATH /home/pablo/dev/CGT/blog/resources/views/welcome.blade.php ENDPATH**/ ?>
