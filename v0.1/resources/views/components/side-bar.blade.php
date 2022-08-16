bea6f743135f2f20bf0a74169a1bef7d780af413
Don Peibol <elcorreodedonpablo@gmail.com>
2022-08-16 10:51:29 +0200

    Pequeños cambios en slider

--- v0.1/resources/views/components/side-bar.blade.php
+++ v0.1/resources/views/components/side-bar.blade.php
@@ -1,6 +1,6 @@
 <div class="sidebar">
   <a class="active" href="#videos">Videos</a>
-  <a href="logos">Logos</a>
+  <a href="#logos">Logos</a>
   <a href="#fotos">Fotos</a>
   <a href="#carteles">Carteles</a>
 </div>
