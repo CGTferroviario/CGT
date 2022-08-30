bb4775374bba6e3083e7c4d19cda6e11a7441c1e
peibol <informaticacgtferroviario@gmail.com>
2022-08-30 14:24:55 +0200

    Quitado blog y he roto varias cosas :(

--- v0.1/resources/views/biblioteca.blade.php
+++ v0.1/resources/views/biblioteca.blade.php
@@ -2,16 +2,70 @@
 
 @section('content')
 
-<x-side-bar-biblio />
 
-{{-- <div class="row">
-   <div class="col-md-6">
-      <img src="/images/COM60RENFE.png" />
-      <p class="blanco"></p>
+
+<div class="row">
+   <div class="col-md-2">
+      <x-side-bar-biblio />
    </div>
-   <div class="col-md-6">
-      <p class="blanco">Esta es la BIBLIOTECA</p>
+   <div class="col-md-5">
+      <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+         <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+         </div>
+         <div class="carousel-inner">
+            <div class="carousel-item active">
+               <img src="/images/COM60RENFE.png" />
+            </div>
+            <div class="carousel-item">
+               <img src="..." class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+               <img src="..." class="d-block w-100" alt="...">
+            </div>
+         </div>
+         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+         </button>
+         <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+         </button>
+      </div>
    </div>
-</div> --}}
+</div>
 @endsection
 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> --}}
+
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+   <div class="carousel-indicators">
+     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+   </div>
+   <div class="carousel-inner">
+     <div class="carousel-item active">
+       <img src="..." class="d-block w-100" alt="...">
+     </div>
+     <div class="carousel-item">
+       <img src="..." class="d-block w-100" alt="...">
+     </div>
+     <div class="carousel-item">
+       <img src="..." class="d-block w-100" alt="...">
+     </div>
+   </div>
+   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+     <span class="visually-hidden">Previous</span>
+   </button>
+   <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+     <span class="carousel-control-next-icon" aria-hidden="true"></span>
+     <span class="visually-hidden">Next</span>
+   </button>
+ </div>
--- v0.1/resources/views/components/side-bar-biblio.blade.php
+++ v0.1/resources/views/components/side-bar-biblio.blade.php
@@ -16,31 +16,21 @@
     <input class="form-check-input" type="checkbox" id="flexSwitchCheckCheckedDisabled" checked>
   </div>
 </div> --}}
-
-
 <div class="sidebar">
-  
   <div class="btn-group">    
     <input type="checkbox" name="colorCheckbox" value="comunicados" checked> 
     <label class="form-check-label" for="flexSwitchCheckDefault"><a class="active" href="#comunicados"><i class="fa-solid fa-bullhorn"></i>Comunicados</a></label>
-    
-    {{-- <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" checked></label> --}}
   </div>
   <div class="btn-group">
     <input type="checkbox" name="colorCheckbox" value="convenios" checked>
     <label class="form-check-label" for="flexSwitchCheckChecked"><a href="#convenios"><i class="fa-solid fa-book"></i>Convenios</a></label>
-    
-    {{-- <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked> --}}
   </div>
   <div class="btn-group">
     <input type="checkbox" name="colorCheckbox" value="leg_laboral" checked>
     <label class="form-check-label" for="flexSwitchCheckDisabled"><a href="#laboral"><i class="fa-solid fa-scale-balanced"></i>Leg. Laboral</a></label>
-    
   </div>
   <div class="btn-group">
     <input type="checkbox" name="colorCheckbox" value="leg_ferroviaria" checked>
     <label class="form-check-label" for="flexSwitchCheckCheckedDisabled"><a href="#ferroviaria"><i class="fa-solid fa-train"></i>Leg. Ferroviaria</a></label>
-    
-    {{-- <input class="form-check-input" type="checkbox" id="flexSwitchCheckCheckedDisabled" checked> --}}
   </div>
 </div>
