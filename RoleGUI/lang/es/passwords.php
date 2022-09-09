412454159b9f3b7dfaa150beb0a791aa59faa771
Don Peibol <elcorreodedonpablo@gmail.com>
2022-09-09 22:06:45 +0200

    Añadidas algunas traducciones a RoleGUI

---
+++ RoleGUI/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => '¡Tu contraseña ha sido reseteada!',
+    'sent' => '¡Hemos enviado un enlace para resetear la contraseña!',
+    'throttled' => 'Por favor, espere antes de reintentarlo.',
+    'token' => 'Este token de reseteo de contraseña es inválido.',
+    'user' => "No hemos encontrado un usuario con esa dirección de correo.",
+
+];
