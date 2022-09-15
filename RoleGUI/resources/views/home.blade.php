d81a8925fb5fdc0a1de925984d5e848cc55af851
peibol <informaticacgtferroviario@gmail.com>
2022-09-15 14:13:37 +0200

    Finalizado gestion de usuarios
    Falta implementar en proyecto general

--- RoleGUI/resources/views/home.blade.php
+++ RoleGUI/resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('¡Estás Logueado!') }}
                 </div>
             </div>
         </div>
