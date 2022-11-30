416cb17c369815c013a8e903b3b871dbbc63ae0f
peibol <informaticacgtferroviario@gmail.com>
2022-11-30 14:07:52 +0100

    Actualizado RENFE componentes

--- v0.1/resources/views/components/renfe/comercial.blade.php
+++ v0.1/resources/views/components/renfe/comercial.blade.php
@@ -1,3 +1,61 @@
-<div>
-    <!-- Let all your things have their places; let each part of your business have its time. - Benjamin Franklin -->
+<div class="jumbotron jumbotron-fluid fondo4">
+    <div class="container">
+        <div class="row py-lg-5">
+            <div class="col-lg-6 col-md-8 mx-auto centrado">
+                <a href="http://cgtferroviario.es" target="blank"><img src="img/logo_med.png"></a>
+                <a href="https://cgt.org.es/" target="blank"><img src="img/logoCGT_med.png"></a>
+                <h1 class="titulo">Documentación Talleres <br> SFFCGT</h1>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container py-5">
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="img/pdf_icon.png" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8 d-flex flex-column">
+                        <div class="card-body">
+                            <h5 class="card-title">Plataforma reivindicativa de CGT para el Colectivo de Comercial/Taquillas</h5>
+                            <p class="card-text">Este documento recoge las reivindicaciones de CGT para el Colectivo de Comercial/Taquillas</p>
+                        </div>
+                        <div class="card-footer d-flex justify-content-between align-items-center">
+                                <div class="col-md-6 centrado">
+                                    <a href="doc/AFILIACION/FICHA_AFILIACION_NOMINA.pdf" class="btn" target="blank"><i class="fa fa-eye"></i> Visualizar</a>
+                                </div>
+                                <div class="col-md-6 centrado">
+                                    <a href="doc/AFILIACION/FICHA_AFILIACION_NOMINA.pdf" target="_blank" download="CGT_Afiliacion_Nomina" class="btn"><i class="fa fa-download"></i> Descargar</a>
+                                </div>
+                            </div>
+                      </div>
+                  </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="img/pdf_icon.png" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8 d-flex flex-column">
+                        <div class="card-body">
+                            <h5 class="card-title">Acuerdo Desarrollo Profesional Colectivo Comercial</h5>
+                            <p class="card-text">Establece y desarrolla el marco específico de relaciones laborales del colectivo de Comerciales y Taquillas.</p>
+                        </div>
+                        <div class="card-footer d-flex justify-content-between align-items-center">
+                            <div class="col-md-6 centrado">
+                                <a href="doc/AFILIACION/FICHA_AFILIACION_NOMINA.pdf" class="btn" target="blank"><i class="fa fa-eye"></i> Visualizar</a>
+                            </div>
+                            <div class="col-md-6 centrado">
+                                <a href="doc/AFILIACION/FICHA_AFILIACION_NOMINA.pdf" target="_blank" download="CGT_Afiliacion_Nomina" class="btn"><i class="fa fa-download"></i> Descargar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
--- v0.1/resources/views/components/renfe/talleres.blade.php
+++ v0.1/resources/views/components/renfe/talleres.blade.php
@@ -55,6 +55,48 @@
                     </div>
                 </div>
             </div>
+            <div class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="img/pdf_icon.png" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8 d-flex flex-column">
+                        <div class="card-body">
+                            <h5 class="card-title">Adaptación del texto normativo de la función de CONDUCCIÓN DE VEHÍCULOS POR CARRETERA por personal de MIT</h5>
+                            <p class="card-text">(Brigada de socorro)</p>
+                        </div>
+                        <div class="card-footer d-flex justify-content-between align-items-center">
+                            <div class="col-md-6 centrado">
+                                <a href="doc/AFILIACION/FICHA_AFILIACION_NOMINA.pdf" class="btn" target="blank"><i class="fa fa-eye"></i> Visualizar</a>
+                            </div>
+                            <div class="col-md-6 centrado">
+                                <a href="doc/AFILIACION/FICHA_AFILIACION_NOMINA.pdf" target="_blank" download="CGT_Afiliacion_Nomina" class="btn"><i class="fa fa-download"></i> Descargar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="img/pdf_icon.png" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8 d-flex flex-column">
+                        <div class="card-body">
+                            <h5 class="card-title">Regulación de la función de ASISTENCIA TÉCNICA e INTERVENCIÓN EN LÍNEA</h5>
+                            <p class="card-text">Material Motor</p>
+                        </div>
+                        <div class="card-footer d-flex justify-content-between align-items-center">
+                            <div class="col-md-6 centrado">
+                                <a href="doc/AFILIACION/FICHA_AFILIACION_NOMINA.pdf" class="btn" target="blank"><i class="fa fa-eye"></i> Visualizar</a>
+                            </div>
+                            <div class="col-md-6 centrado">
+                                <a href="doc/AFILIACION/FICHA_AFILIACION_NOMINA.pdf" target="_blank" download="CGT_Afiliacion_Nomina" class="btn"><i class="fa fa-download"></i> Descargar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="col-sm-6">
             <div class="card mb-3">
@@ -87,6 +129,27 @@
                         <div class="card-body">
                             <h5 class="card-title">Movimientos y Operaciones con vehículos en vías de servicio e instalaciones en TALLERES</h5>
                             <p class="card-text">Mejorar la operatividad de los medios técnicos y humanos disponibles en sus centros de trabajo en lo que respecta al mantenimiento y puesta a disposición de los vehículos.</p>
+                        </div>
+                        <div class="card-footer d-flex justify-content-between align-items-center">
+                            <div class="col-md-6 centrado">
+                                <a href="doc/AFILIACION/FICHA_AFILIACION_NOMINA.pdf" class="btn" target="blank"><i class="fa fa-eye"></i> Visualizar</a>
+                            </div>
+                            <div class="col-md-6 centrado">
+                                <a href="doc/AFILIACION/FICHA_AFILIACION_NOMINA.pdf" target="_blank" download="CGT_Afiliacion_Nomina" class="btn"><i class="fa fa-download"></i> Descargar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="img/pdf_icon.png" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8 d-flex flex-column">
+                        <div class="card-body">
+                            <h5 class="card-title">Plataforma Reivindicativa CGT - Talleres</h5>
+                            <p class="card-text">Esta es la plataforma que recoge las reivindicaciones de CGT para el ámbito de los Talleres</p>
                         </div>
                         <div class="card-footer d-flex justify-content-between align-items-center">
                             <div class="col-md-6 centrado">
