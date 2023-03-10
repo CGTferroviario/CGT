4e39836d8906f266396c206f959c602701db83db
peibol <informaticacgtferroviario@gmail.com>
2023-03-10 14:03:39 +0100

    Añadidas variables para que funcione el formulario de correo

--- v0.1/resources/views/equipo/contacto.blade.php
+++ v0.1/resources/views/equipo/contacto.blade.php
@@ -1,3 +1,11 @@
+<style>
+    /* MAIL_MAILER=smtp
+    MAIL_HOST=ssl0.ovh.net
+    MAIL_PORT=465
+    MAIL_USERNAME=admin@cgtferroviario.es
+    MAIL_PASSWORD=93nLqHHzPaA9nea
+    MAIL_ENCRYPTION=tls */
+</style>
 @extends('layouts.master')
 
 @section('content')
