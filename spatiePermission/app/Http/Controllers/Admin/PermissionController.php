9bc8fd06519bf561eaf90e9651178a624dc38773
peibol <informaticacgtferroviario@gmail.com>
2023-04-21 13:50:25 +0200

    Mejorado permisos

--- spatiePermission/app/Http/Controllers/Admin/PermissionController.php
+++ spatiePermission/app/Http/Controllers/Admin/PermissionController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
 
 class PermissionController extends Controller
 {
-    //
+    public function index()
+    {
+        $permissions = Permission::all();
+        return view('admin.permissions.index', compact('permissions'));
+    }
 }
