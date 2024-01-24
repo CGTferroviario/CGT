87d5765a94cbee89f8922bcae8eecf22f12f5f41
peibol <informaticacgtferroviario@gmail.com>
2024-01-24 18:20:30 +0100

    Casi bien  el seeder de comunicados

--- beta/database/seeders/ComunicadoSeeder.php
+++ beta/database/seeders/ComunicadoSeeder.php
@@ -20,44 +20,37 @@ class ComunicadoSeeder extends Seeder
 
         $csvFile = fopen(base_path("database/data/comunicados.csv"), "r");
 
-        $firstline = true;
-        while (($data = fgetcsv($csvFile, 555, ";")) !== false) {
-            if (!$firstline) {
-                
-                $fecha = DateTime::createFromFormat('m/d/Y', mb_convert_encoding($data['8'], 'UTF-8', 'ISO-8859-1'));
-                if ($fecha !== false) {
-                    $fecha = $fecha->format('Y-m-d');
-                } else {
-                    // Handle error
-                }
-                $creado = Carbon::createFromFormat('d/m/Y H:i', $data['15']);
-                $actualizado = Carbon::createFromFormat('d/m/Y H:i', $data['16']);
+        $fields = ['id', 'visualizaciones', 'descargas', 'publicado', 'user_id', 'numero', 'empresa_id', 'categoria_id', 'fecha', 'titulo', 'subtitulo', 'cuerpo', 'pdf', 'imagen', 'adjunto', 'created_at', 'updated_at'];
 
-                $comunicado = Comunicado::create([
-                    "id" => mb_convert_encoding($data['0'], 'UTF-8', 'ISO-8859-1'),
-                    "visualizaciones" => mb_convert_encoding($data['1'], 'UTF-8', 'ISO-8859-1'),
-                    "descargas" => mb_convert_encoding($data['2'], 'UTF-8', 'ISO-8859-1'),                  
-                    "publicado" => mb_convert_encoding($data['3'], 'UTF-8', 'ISO-8859-1'),
-                    "user_id" => mb_convert_encoding($data['4'], 'UTF-8', 'ISO-8859-1'),
-                    "numero" => mb_convert_encoding($data['5'], 'UTF-8', 'ISO-8859-1'),
-                    "empresa_id" => mb_convert_encoding($data['6'], 'UTF-8', 'ISO-8859-1'),
-                    "categoria_id" => mb_convert_encoding($data['7'], 'UTF-8', 'ISO-8859-1'),
-                    "fecha" => $fecha,
-                    "titulo" => mb_convert_encoding($data['9'], 'UTF-8', 'ISO-8859-1'),
-                    "subtitulo" => mb_convert_encoding($data['10'], 'UTF-8', 'ISO-8859-1'),
-                    "cuerpo" => mb_convert_encoding($data['11'], 'UTF-8', 'ISO-8859-1'),
-                    "pdf" => mb_convert_encoding($data['12'], 'UTF-8', 'ISO-8859-1'),
-                    "imagen" => mb_convert_encoding($data['13'], 'UTF-8', 'ISO-8859-1'),
-                    "adjunto" => mb_convert_encoding($data['14'], 'UTF-8', 'ISO-8859-1'),
-                    "created_at" => $creado,
-                    "updated_at" => $actualizado,
-                    // "created_at" => mb_convert_encoding($data['15'], 'UTF-8', 'ISO-8859-1'),
-                    // "updated_at" => mb_convert_encoding($data['16'], 'UTF-8', 'ISO-8859-1'),
-                ]);
+        while (($data = fgetcsv($csvFile, 555, ";")) !== false) {
+            if ($data === array_slice($data, 0, 1)) {
+                continue;
             }
-            $firstline = false;
+
+            $row = [];
+            foreach ($fields as $index => $field) {
+                $row[$field] = mb_convert_encoding($data[$index], 'UTF-8', 'ISO-8859-1');
+            }
+
+            $fecha = DateTime::createFromFormat('m/d/Y', $row['fecha']);
+            if ($fecha !== false) {
+                $row['fecha'] = $fecha->format('Y-m-d');
+            } else {
+                // Handle error
+            }
+
+            // $creado = Carbon::createFromFormat('d/m/Y H:i', $data['15']);
+            // $actualizado = Carbon::createFromFormat('d/m/Y H:i', $data['16']);
+
+            
+
+            $row['created_at'] = Carbon::createFromFormat('d/m/Y H:i', $data['15']);
+            $row['updated_at'] = Carbon::createFromFormat('d/m/Y H:i', $data['16']);
+
+            Comunicado::create($row);
         }
 
         fclose($csvFile);
+
     }
 }
