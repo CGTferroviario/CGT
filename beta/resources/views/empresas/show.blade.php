44d07b74a2c3516b844c7ce32605c1104d2db97d
Don Peibol <elcorreodedonpablo@gmail.com>
2024-02-12 17:09:13 +0100

    Minicambios a empresas

--- beta/resources/views/empresas/show.blade.php
+++ beta/resources/views/empresas/show.blade.php
@@ -7,22 +7,21 @@
         </x-slot>
         <div class="p-8 fondo comunicados">
             <div id="accordion-collapse" data-accordion="collapse">
-                <h2 id="encabezado-comunicados">
+                <h2 class="text-zinc-200" id="encabezado-comunicados">
                     <button type="button"
-                        class="flex items-center justify-between w-full p-5 font-medium rounded-t-xl rtl:text-right border border-b-0 focus:ring-1 focus:ring-zinc-800 border-zinc-700 text-zinc-800 bg-red-500 hover:bg-red-500 hover:text-zinc-800 gap-3"
+                        class="flex items-center justify-between w-full p-5 font-medium rounded-t-xl rtl:text-right border border-b-0 focus:ring-1 focus:ring-zinc-800 border-zinc-700 text-zinc-200 group-hover:text-white bg-red-500 hover:bg-red-500 hover:text-zinc-800 gap-3"
                         data-accordion-target="#acordeon-comunicados" aria-expanded="true"
                         aria-controls="acordeon-comunicados">
                         <span>Comunicados {{ $comunicados->count() }}</span>
-                        <i class="lni lni-chevron-down ml-1 mt-1 text-red-500 group-hover:text-white"></i>
+                        <i class="lni lni-chevron-down ml-1 mt-1 text-red-500 group-hover:text-black active:text-black"></i>
                     </button>
                 </h2>
                 <div id="acordeon-comunicados" class="hidden" aria-labelledby="encabezado-comunicados">
-                    <div class="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-4 m-6 rounded-lg">
-                        <div class="col-span-4 paginacion">{{ $comunicados->links('vendor.pagination.tailwind') }}</div>
+                    <div class="w-full text-sm md:text-xs p-2 paginacion">{{ $comunicados->links('vendor.pagination.tailwind') }}</div>
+                    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 m-6 rounded-lg">
                         @foreach ($comunicados as $comunicado)                                
                             <x-comunicado :comunicado=$comunicado></x-comunicado>
                         @endforeach
-                        <div class="col-span-4 paginacion">{{ $comunicados->links('vendor.pagination.tailwind') }}</div>
                     </div>
                 </div>
                 <h2 id="encabezado-noticias">
