2d8815e812a73b2edffa1c39ce65d1d722852753
peibol <informaticacgtferroviario@gmail.com>
2024-02-22 12:33:57 +0100

    Probando cosas con los csv

--- beta/database/seeders/NoticiaSeeder.php
+++ beta/database/seeders/NoticiaSeeder.php
@@ -17,9 +17,7 @@ class NoticiaSeeder extends Seeder
      */
     public function run(): void
     {
-        Noticia::truncate();
-
-        FacadesExcel::import(new NoticiasImport, 'database/data/noticias.csv');
+        // Noticia::truncate();
 
         // $csvFile = fopen(base_path("database/data/noticias.csv"), "r");
 
@@ -59,41 +57,46 @@ class NoticiaSeeder extends Seeder
 
         // fclose($csvFile);
 
-        // Noticia::truncate();
 
-        // $csvFile = fopen(base_path("database/data/noticias.opt.csv"), "r");
+        // FacadesExcel::import(new NoticiasImport, 'database/data/noticias.csv');
 
-        // $firstline = true;
-        // while (($data = fgetcsv($csvFile, 555, ";")) !== false) {
-        //     if (!$firstline) {
 
-        //         $fecha = DateTime::createFromFormat('m/d/Y', $data['6']);
-        //         if ($fecha !== false) {
-        //             $row['6'] = $fecha->format('Y-m-d');
-        //         } else {
-        //             // Handle error
-        //         }
 
-        //         $Noticia = Noticia::create([
-        //             "id" => mb_convert_encoding($data['0'], 'UTF-8', 'ISO-8859-1'),
-        //             "visualizaciones" => mb_convert_encoding($data['1'], 'UTF-8', 'ISO-8859-1'),
-        //             "publicado" => mb_convert_encoding($data['2'], 'UTF-8', 'ISO-8859-1'),                  
-        //             "user_id" => mb_convert_encoding($data['3'], 'UTF-8', 'ISO-8859-1'),
-        //             "empresa_id" => mb_convert_encoding($data['4'], 'UTF-8', 'ISO-8859-1'),
-        //             "categoria_id" => mb_convert_encoding($data['5'], 'UTF-8', 'ISO-8859-1'),
-        //             "fecha" => $fecha,
-        //             "titulo" => mb_convert_encoding($data['7'], 'UTF-8', 'ISO-8859-1'),
-        //             "slug" => \Illuminate\Support\Str::slug($data['7']),
-        //             "subtitulo" => mb_convert_encoding($data['9'], 'UTF-8', 'ISO-8859-1'),
-        //             "cuerpo" => mb_convert_encoding($data['10'], 'UTF-8', 'ISO-8859-1'),
-        //             "adjunto" => mb_convert_encoding($data['11'], 'UTF-8', 'ISO-8859-1'),
-        //             "imagen" => mb_convert_encoding($data['12'], 'UTF-8', 'ISO-8859-1'),
-        //         ]);
-        //     }
-        //     $firstline = false;
-        // }
+        Noticia::truncate();
 
-        // fclose($csvFile);
+        $csvFile = fopen(base_path("database/data/noticias.csv"), "r");
+
+        $firstline = true;
+        while (($data = fgetcsv($csvFile, 555, ";")) !== false) {
+            if (!$firstline) {
+
+                $fecha = DateTime::createFromFormat('m/d/Y', $data['6']);
+                if ($fecha !== false) {
+                    $row['6'] = $fecha->format('Y-m-d');
+                } else {
+                    // Handle error
+                }
+
+                $Noticia = Noticia::create([
+                    "id" => mb_convert_encoding($data['0'], 'UTF-8', 'ISO-8859-1'),
+                    "visualizaciones" => mb_convert_encoding($data['1'], 'UTF-8', 'ISO-8859-1'),
+                    "publicado" => mb_convert_encoding($data['2'], 'UTF-8', 'ISO-8859-1'),                  
+                    "user_id" => mb_convert_encoding($data['3'], 'UTF-8', 'ISO-8859-1'),
+                    "empresa_id" => mb_convert_encoding($data['4'], 'UTF-8', 'ISO-8859-1'),
+                    "categoria_id" => mb_convert_encoding($data['5'], 'UTF-8', 'ISO-8859-1'),
+                    "fecha" => $fecha,
+                    "titulo" => mb_convert_encoding($data['7'], 'UTF-8', 'ISO-8859-1'),
+                    "slug" => \Illuminate\Support\Str::slug($data['7']),
+                    "subtitulo" => mb_convert_encoding($data['9'], 'UTF-8', 'ISO-8859-1'),
+                    "cuerpo" => mb_convert_encoding($data['10'], 'UTF-8', 'ISO-8859-1'),
+                    "adjunto" => mb_convert_encoding($data['11'], 'UTF-8', 'ISO-8859-1'),
+                    "imagen" => mb_convert_encoding($data['12'], 'UTF-8', 'ISO-8859-1'),
+                ]);
+            }
+            $firstline = false;
+        }
+
+        fclose($csvFile);
 
     //     Noticia::truncate();
 
