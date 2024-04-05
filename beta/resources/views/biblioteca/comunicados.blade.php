e34ba890cd340940a0d8b37e39cac6c19eb1d709
peibol <informaticacgtferroviario@gmail.com>
2024-04-05 14:02:12 +0200

    Añadido registro de horas

--- beta/resources/views/biblioteca/comunicados.blade.php
+++ beta/resources/views/biblioteca/comunicados.blade.php
@@ -2,18 +2,19 @@
     @section('contenido')
         <h1 class="m-0 py-3 text-center bg-red-500 font-bold leading-tight titular">COMUNICADOS</h1>
         <div class="fondo comunicados">
-            <div class="md:w-1/2 sm:w-full mx-auto">
+            <div class="sm:w-full md:w-2/3 mx-auto">
                 <x-buscador :empresas=$empresas :categorias=$categorias :etiquetas=$etiquetas></x-buscador>
             </div>
             
-            <div class="w-full px-8">{{ $comunicados->onEachSide(2)->links() }}</div>
+            
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4 p-4 m-6 bg-blanco-transp bordeRojo rounded-lg">
-
+                <div class="col-span-1 md:col-span-2 lg:col-span-2 xl:col-span-3">{{ $comunicados->onEachSide(2)->links() }}</div>
                 @foreach ($comunicados as $comunicado)
                     <x-comunicado :comunicado=$comunicado></x-comunicado>
                 @endforeach
+                <div class="col-span-3">{{ $comunicados->links('vendor.pagination.tailwind') }}</div>
             </div>
-            <div class="w-full paginacion">{{ $comunicados->links('vendor.pagination.tailwind') }}</div>
+            
         </div>
     @endsection
 </x-publico-layout>
